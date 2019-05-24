749825d652b4295f4122fa27a1815ef55c1ef939
Nick Evans <nicholasevans17@gmail.com>
2019-05-24 15:55:47 -0700

    add random color to tiles on mouse hover

---
+++ scripts.js
@@ -0,0 +1,30 @@
+
+
+for (x = 0; x < 16; x++) {
+  for (y = 0; y < 16; y++) {
+      let div = document.createElement("div");
+      div.textContent = "    ";
+      div.setAttribute('style', 'width: 30px; height: 30px; border: 1px solid black')
+      document.getElementById("container").appendChild(div).addEventListener("mouseover", function(){
+        function rg(m, n) {
+          return Math.floor( Math.random() * (n - m + 1) ) + m;
+        }
+        
+        function hex(i) {
+          return i.toString(16);
+        }
+        
+        function randColor() {
+          return '#' + hex(rg(1, 15)) + hex(rg(1, 15)) + hex(rg(1, 15)) +
+              hex(rg(1, 15)) + hex(rg(1, 15)) + hex(rg(1, 15));
+        }
+        
+        div.style.backgroundColor = randColor()      
+      });
+     
+  }
+
+  var jump = document.createElement("br");
+  document.getElementById("container").appendChild(jump);
+
+}
