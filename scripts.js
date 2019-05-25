87b6a65ad0490d00d7c2ea79c6eb1fcf29fe84bd
Nick Evans <nicholasevans17@gmail.com>
2019-05-24 17:26:07 -0700

    change background color to blue

--- scripts.js
+++ scripts.js
@@ -1,9 +1,6 @@
-
-
 for (x = 0; x < 16; x++) {
   for (y = 0; y < 16; y++) {
       let div = document.createElement("div");
-      div.textContent = "    ";
       div.setAttribute('style', 'width: 30px; height: 30px; border: 1px solid black')
       document.getElementById("container").appendChild(div).addEventListener("mouseover", function(){
         function rg(m, n) {
@@ -28,3 +25,11 @@ for (x = 0; x < 16; x++) {
   document.getElementById("container").appendChild(jump);
 
 }
+
+  button.addEventListener("click", function(){
+  let reset = window.prompt("Enter the Number of Squares to Make a New Grid!", 16);
+ 
+  
+
+  reset;
+});
